4f95ef5154d94fbe6685fd920d8c8899790cada6
Jason Carr <jason.carr@althouse.co>
2015-09-18 11:10:35 -0700

    Made Foster Care text updates (now Parent Aide Program page)

--- parent-aide.php
+++ parent-aide.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Foster Care - Black Family and Child Services of Arizona</title>
+    <title>Parent Aide Program - Black Family and Child Services of Arizona</title>
     <link rel="stylesheet" href="css/foundation.css">
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/responsive-nav.css">
@@ -22,8 +22,8 @@
 	  	
    	  <div class="row service-info hide-for-large-up">
 	  		<div class="large-6 columns">
-	  			<h3>Foster Care</h3>
-	  			<h4>Need sub-headline</h4>
+	  			<h3>Parent Aide Program</h3>
+	  			<h4>Improving parenting skills for families referred by DCS</h4>
 	  		</div>
 	  </div>
   
@@ -70,8 +70,8 @@
 	  <section class="service-info">
 	  	<div class="row">
 	  		<div class="large-6 columns show-for-large-up">
-	  			<h3>Foster Care</h3>
-	  			<h4>Need sub-headline</h4>
+	  			<h3>Parent Aide Program</h3>
+	  			<h4>Improving parenting skills for families referred by DCS</h4>
 	  		</div>
 	  		<div class="large-3 small-6 columns service-video">
 	  			<a class="padding-left" href="#" data-reveal-id="modalOne"><img src="img/placeholder-four.jpg" alt=""></a>
@@ -84,37 +84,19 @@
 	  	</div>
 	  	<div class="row">
 	  		<div class="large-5 columns">
-	  			<p>One of Black Family and Child Services goals is to help place children with special needs in safe, happy and healthy environments.</p>
-	  			<p>This placement may be temporary— in a foster home with the goal of reuniting the child with birth parent(s) or an extended family member (grandparent, aunt or uncle) or a permanent placement with an adoptive family that would provide a forever home.</p>
-	  			<p>Foster parents provide temporary care to a child when his or her birth parents are unable or unwilling to provide adequate care. There are four major reasons why most foster children need this type of care:</p>
-	  				<ul>
-		  				<li>They are victims of physical, sexual or emotional abuse</li>
-		  				<li>They are victims of serious neglect</li>
-		  				<li>They have been abandoned</li>
-		  				<li>Rights for their care are voluntarily relinquished by their parents</li>
-	  				</ul>
-	  			<p><em>BFCS Foster Care Services provide a comprehensive program that includes</em></p>	
-	  				<ul>
-		  				<li>Orientation - held monthly</li>
-		  				<li>Pre-services training classes</li>
-		  				<li>Foster-adopt program aimed at increasing the number of children in foster care who will be formally adopted by their foster families</li>
-		  				<li>Support groups for grandparents</li>
-		  				<li>Advanced training</li>
-	  				</ul>
+	  			<p>The BFCS Parent Aide Program provides contracted services to families referred by the Arizona Department of Child Safety (DCS) who are seeking reunification with their children.</p>
+	  			<p>Parents learn how to improve their parenting skills and gain ability to provide their children with a safe and secure home environment.</p>
+	  			<p>Parent Aides work with parents to set realistic goals, monitor and review their  progress in achieving needed behavioral changes, and provide reports and recommendations to DCS that assist in decisions about reunification.</p>
 	  		</div>
 	  		<div class="large-5 columns">
-	  			<p><em>Foster Care Requirements</em></p>
+	  			<p><em>Services provided include:</em></p>
 	  				<ul>
-		  				<li>You must be over 21 years of age</li>
-		  				<li>You can be either married or single</li>
-		  				<li>Persons in your home 18 years of age or older must be fingerprinted and submit a current physician’s report</li>
-		  				<li>Parents must attend 30 hours of foster care training classes</li>
-	  				</ul>
-	  			<p><em>You may qualify as a foster parent even if</em></p>	
-	  				<ul>
-		  				<li>You do not own your own home</li>
-		  				<li>You do not have a separate room for your foster child—a foster child can stay in your room in a crib up to age three</li>
-		  				<li>Your income is modest and you do not have a bank account</li>
+		  				<li>Parenting Education – weekly individual sessions on topics such as nurturing, child development, appropriate discipline and communication</li>
+		  				<li>Coping Skills Training – personal problem solving, stress management</li>
+		  				<li>Life Skills Training – including home management, budgeting and nutrition</li>
+		  				<li>Maintaining a safe and stable home environment</li>
+		  				<li>Supervised visits – including transportation of children to visits</li>
+		  				<li>Linking families to needed community resources</li>
 	  				</ul>
 	  		</div>	  		
 	  	</div>	  	
